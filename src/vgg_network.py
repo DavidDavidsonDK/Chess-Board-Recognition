e6720fa1855bd8ddeb0fb9f681221fe44ef6af04
[Davit] <[d.kprogrammer0.0@gmail.com]>
2018-11-11 22:26:11 +0400

    Fix bug

--- src/vgg_network.py
+++ src/vgg_network.py
@@ -7,34 +7,34 @@ from keras.layers import Dense, Dropout, Flatten,Input, Reshape
 import numpy as np
 
 class VGG_Network(Model):
-    def __init__(self,
-                 input_dim, 
-                 output_classes,
-                 last_freez_layers = -4,
-                 dropout = 0.5):
+	def __init__(self,
+				 input_dim, 
+				 output_classes,
+				 last_freez_layers = -4,
+				 dropout = 0.5):
 
-    	self.dim = input_dim
-    	self.classes = output_classes
-    	self.dropout = dropout
+		self.dim = input_dim
+		self.classes = output_classes
+		self.dropout = dropout
 
-        X = Input(shape=input_dim, name='X')
-        inputs = [X]
+		X = Input(shape=input_dim, name='X')
+		inputs = [X]
 
-        print('Download_vgg')
-        vgg_conv = VGG16(weights='imagenet',include_top=False,input_shape=input_dim)
-        print('Pop last two dense layers')
+		print('Download_vgg')
+		vgg_conv = VGG16(weights='imagenet',include_top=False,input_shape=input_dim)
+		print('Pop last two dense layers')
 
-        print('Freeze first layers')
-        for layer in vgg_conv.layers[:last_freez_layers]:
-            layer.trainable = False
+		print('Freeze first layers')
+		for layer in vgg_conv.layers[:last_freez_layers]:
+			layer.trainable = False
 
-        X = vgg_conv(X)
-        X = Flatten()(X)
-        X = Dense(512, activation='relu')(X)
-        X = Dropout(dropout)(X)
-        y = Dense(output_classes, activation='softmax')(X)
-        outputs = [y]
-        super(Network, self).__init__(inputs=inputs, outputs=outputs)
+		X = vgg_conv(X)
+		X = Flatten()(X)
+		X = Dense(512, activation='relu')(X)
+		X = Dropout(dropout)(X)
+		y = Dense(output_classes, activation='softmax')(X)
+		outputs = [y]
+		super(Network, self).__init__(inputs=inputs, outputs=outputs)
 
-    def say_name(self):
-        pass
+	def say_name(self):
+		pass
